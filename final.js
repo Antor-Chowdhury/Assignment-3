60ef03de7d656fc4cfab804e82e5dd6abf874b81
Antor-Chowdhury <antor1347@gmail.com>
2025-08-04 04:18:19 +0600

    added problem-5.js in Assignment-3

--- final.js
+++ final.js
@@ -36,3 +36,10 @@ if (fileName.includes("#")) {
 } else {
   console.log("Delete");
 }
+
+/** Problem 05 - ( PH Email Generator )  */
+var student = { name: "jhankar", roll: 1014, department: "cse" };
+//write your code here
+console.log(
+  student.name + student.roll + "." + student.department + "@ph.ac.bd"
+);
