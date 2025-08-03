1f0f816fd6a6b27dac9c8b9b8788d027cd08923e
Antor-Chowdhury <antor1347@gmail.com>
2025-08-04 02:47:49 +0600

    added problem-3.js in Assignment-3

--- final.js
+++ final.js
@@ -3,3 +3,25 @@ var area = 800;
 //write your code here
 area = area / 2;
 console.log(area);
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+if (money >= 25000) {
+  console.log("Laptop");
+} else if (money >= 10000) {
+  console.log("Cycle");
+} else {
+  console.log("Chocolate");
+}
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 11;
+//write your code here
+for (i = 1; i <= lastDay; i++) {
+  if (i % 3 === 0) {
+    console.log(i + " - " + "medicine");
+  } else {
+    console.log(i + " - " + "rest");
+  }
+}
