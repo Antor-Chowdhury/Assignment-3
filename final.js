01b5967ad547e68b53096e5f16cffd78fc9e4356
Antor-Chowdhury <antor1347@gmail.com>
2025-08-04 04:08:13 +0600

    added problem-4.js in Assignment-3

--- final.js
+++ final.js
@@ -25,3 +25,14 @@ for (i = 1; i <= lastDay; i++) {
     console.log(i + " - " + "rest");
   }
 }
+
+/** Problem 04 - (Delete / Store) */
+var fileName = "pdfData.jpg";
+//write your code here
+if (fileName.includes("#")) {
+  console.log("Store");
+} else if (fileName.slice(-4) === ".pdf" || fileName.slice(-5) === ".docx") {
+  console.log("Store");
+} else {
+  console.log("Delete");
+}
