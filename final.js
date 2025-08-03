9e259fb7360edb14f102e0da7e8242ca74150870
Antor-Chowdhury <antor1347@gmail.com>
2025-08-04 04:56:33 +0600

    add problem-6.js in Assignment-3

--- final.js
+++ final.js
@@ -43,3 +43,13 @@ var student = { name: "jhankar", roll: 1014, department: "cse" };
 console.log(
   student.name + student.roll + "." + student.department + "@ph.ac.bd"
 );
+
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+//write your code here
+for (i = 1; i <= experience; i++) {
+  var salaryIncrease = (startingSalary * 5) / 100;
+  startingSalary = startingSalary + salaryIncrease;
+}
+console.log(startingSalary.toFixed(2));
